99257cb462c171871340d5dfaa25ce520a6c1210
Robert Gambee <robert@futuresearch.ai>
2026-01-21 18:54:36 -0800

    Regenerate types after forbidding extra props

--- src/everyrow/generated/models/dedupe_public_params.py
+++ src/everyrow/generated/models/dedupe_public_params.py
@@ -4,7 +4,6 @@ from collections.abc import Mapping
 from typing import Any, TypeVar
 
 from attrs import define as _attrs_define
-from attrs import field as _attrs_field
 
 from ..types import UNSET, Unset
 
@@ -25,7 +24,6 @@ class DedupePublicParams:
     equivalence_relation: str
     select_representative: bool | Unset = True
     preview: bool | Unset = False
-    additional_properties: dict[str, Any] = _attrs_field(init=False, factory=dict)
 
     def to_dict(self) -> dict[str, Any]:
         equivalence_relation = self.equivalence_relation
@@ -35,7 +33,7 @@ class DedupePublicParams:
         preview = self.preview
 
         field_dict: dict[str, Any] = {}
-        field_dict.update(self.additional_properties)
+
         field_dict.update(
             {
                 "equivalence_relation": equivalence_relation,
@@ -63,21 +61,4 @@ class DedupePublicParams:
             preview=preview,
         )
 
-        dedupe_public_params.additional_properties = d
         return dedupe_public_params
-
-    @property
-    def additional_keys(self) -> list[str]:
-        return list(self.additional_properties.keys())
-
-    def __getitem__(self, key: str) -> Any:
-        return self.additional_properties[key]
-
-    def __setitem__(self, key: str, value: Any) -> None:
-        self.additional_properties[key] = value
-
-    def __delitem__(self, key: str) -> None:
-        del self.additional_properties[key]
-
-    def __contains__(self, key: str) -> bool:
-        return key in self.additional_properties
